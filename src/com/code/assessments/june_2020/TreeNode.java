e1823e2f69b28dc135aeb9bb22e988a605bdd038
Nayan Kakati <nayan25121989@gmail.com>
2020-06-25 21:10:42 +0530

    CodeSample for June 2020 commit

---
+++ src/com/code/assessments/june_2020/TreeNode.java
@@ -0,0 +1,12 @@
+package com.nayan.code.assessments.june_2020;
+
+public class TreeNode {
+    TreeNode left;
+    TreeNode right;
+    int data;
+
+    public TreeNode(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
